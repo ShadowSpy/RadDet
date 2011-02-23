9e938bef1c69939a6f09f1e0561a210a8b028967
chencaustin <chencaustin@gmail.com>
2011-02-23 02:50:28 -0800

    new branch

--- src/edu/ucla/raddet/Menu.java
+++ src/edu/ucla/raddet/Menu.java
@@ -1,52 +1,194 @@
 package edu.ucla.raddet;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import edu.ucla.raddet.radiation.IDataCollector;
+import edu.ucla.raddet.radiation.RadDetService;
 
 public class Menu extends Activity {
 	
-	private IDataCollector dataCollector = null;
-	
-	/** Saves the connection to the data collector service */
-	private ServiceConnection conn = new ServiceConnection() {
-
-		//@Override
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			dataCollector = IDataCollector.Stub.asInterface(service);
-			try {
-				dataCollector.startDataCollection();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		//@Override
-		public void onServiceDisconnected(ComponentName className) {
-			dataCollector = null;
-		}
-	};
-	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.main);                      
+        
+        //just pass the reference to the service
+        RadDetService.setMainActivity(this);
+    	
+        //creating an intent for the service
+        final Intent RadDetService = new Intent(this, RadDetService.class);
+        
+        //start button
+        Button btnStart = (Button) findViewById(android.R.id.button1);
+        //start button on click
+        btnStart.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {     
+                //start the service
+                startService(RadDetService);
+                
+                //remove or hide the app
+                finish();                
+            }
+        });
+        
+        //stop button
+        Button btnStop = (Button) findViewById(android.R.id.closeButton);
+
+        //on click of stop button
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {             
+                //stop the service
+                stopService(RadDetService);  
+
+                //remove or hide the app
+                finish(); 
+            }
+        });
+        
+        /*@Override 
+        protected void onDestroy() {
+              super.onDestroy();
+        }*/
+        
+    }
+	
+
+/*	//LocationService class// 
+	public class LocalService extends Service {
+	
+		private NotificationManager mNM;
+		
+		public class LocalBinder extends Binder {
+			LocalService getService(){
+				return LocalService.this;
+			}
+		}
+		
+		@Override
+		public void onCreate(){
+			mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+			showNotification();
+		}
+		
+	    @Override
+	    public int onStartCommand(Intent intent, int flags, int startId) {
+	        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+	        // We want this service to continue running until it is explicitly
+	        // stopped, so return sticky.
+	        return START_STICKY;
+	    }
+	    
+	    //TODO: Check cancel(1) and the 0 in toast
+	    @Override
+	    public void onDestroy() {
+	        // Cancel the persistent notification.
+	        mNM.cancel(1);
+	        
+	        // Tell the user we stopped.
+	        Toast.makeText(this, 0, Toast.LENGTH_SHORT).show();
+	    }
+		
+	    @Override
+	    public IBinder onBind(Intent intent) {
+	        return mBinder;
+	    }
+	    
+	    private final IBinder mBinder = new LocalBinder();
+	    
+		private void showNotification(){
+			System.out.println("Notification here:");
+		}
+	}
+	//End of LocationService Class
+	
+	
+	//Start Service
+	private LocalService mBoundService;
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service){
+		
+	        // This is called when the connection with the service has been
+	        // established, giving us the service object we can use to
+	        // interact with the service.  Because we have bound to a explicit
+	        // service that we know is running in our own process, we can
+	        // cast its IBinder to a concrete class and directly access it.
+			mBoundService = ((LocalService.LocalBinder)service).getService();
+			
+			//Text to user
+			Toast.makeText(Binding.this, R.string.local_service_connected,
+					Toast.LENGTH_SHORT).show();
+		}
+	};*/
+	
+	
+
+/*    @Override
+    public void onCreate(Bundle savedInstanceState) {
     	String str;
-    	if (!bindService(new Intent(IDataCollector.class.getName()), conn, Context.BIND_AUTO_CREATE)) {
-    		System.out.println("Could not bind to data collector service");
-    		str = "hello, fail";
-    	}
-    	else {
-    		str = "Hello, Android";
-    	}
+ 
+    	str = "Hello, Android";
+
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
         tv.setText(str);
         setContentView(tv);
-    }
+    }*/
+
 }
+
+
+
+
+
+
+
+
+
+
+
+/*private IDataCollector dataCollector = null;
+
+*//** Saves the connection to the data collector service *//*
+private ServiceConnection conn = new ServiceConnection() {
+
+	//@Override
+	public void onServiceConnected(ComponentName className, IBinder service) {
+		dataCollector = IDataCollector.Stub.asInterface(service);
+		try {
+			dataCollector.startDataCollection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//@Override
+	public void onServiceDisconnected(ComponentName className) {
+		dataCollector = null;
+	}
+};
+
+*//** Called when the activity is first created. *//*
+@Override
+public void onCreate(Bundle savedInstanceState) {
+	String str;
+	if (!bindService(new Intent(IDataCollector.class.getName()), conn, Context.BIND_AUTO_CREATE)) {
+		System.out.println("Could not bind to data collector service");
+		str = "hello, fail";
+	}
+	else {
+		str = "Hello, Android";
+	}
+    super.onCreate(savedInstanceState);
+    TextView tv = new TextView(this);
+    tv.setText(str);
+    setContentView(tv);
+}*/
---
+++ src/edu/ucla/raddet/radiation/RadDetService.java
@@ -0,0 +1,107 @@
+package edu.ucla.raddet.radiation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import edu.ucla.raddet.Menu;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class RadDetService extends Service 
+{
+	public static Menu MAIN_ACTIVITY;
+	
+	private Timer timer=new Timer(); 
+	 
+    private static long UPDATE_INTERVAL = 1*60*1000;  //default
+
+    private static long DELAY_INTERVAL = 0;  
+    
+    // hooks main activity here    
+    public static void setMainActivity(Menu activity) 
+    {
+      MAIN_ACTIVITY = activity;      
+    }
+    
+    /* 
+     * not using ipc...but if we use in future
+     */
+    public IBinder onBind(Intent intent) {
+      return null;
+    }
+    
+    @Override 
+    public void onCreate() 
+    {
+      super.onCreate();     
+
+      _startService();
+
+      if (MAIN_ACTIVITY != null)  Log.d(getClass().getSimpleName(), "RadDetService started");
+    }
+    
+    @Override 
+    public void onDestroy() 
+    {
+      super.onDestroy();
+
+      _shutdownService();
+
+      if (MAIN_ACTIVITY != null)  Log.d(getClass().getSimpleName(), "FileScannerService stopped");
+    }
+    
+    /*
+     * starting the service
+     */
+    private void _startService()
+    {      
+      timer.scheduleAtFixedRate(    
+          
+              new TimerTask() {
+
+                    public void run() {
+
+                        try{
+
+                        doServiceWork();
+
+                        Thread.sleep(UPDATE_INTERVAL);
+
+                        }catch(InterruptedException ie){
+
+                            Log.e(getClass().getSimpleName(), "RadDetService InterruptedException"+ie.toString());
+                        }
+                        
+                    }
+                  },
+                  DELAY_INTERVAL,
+                  UPDATE_INTERVAL);
+
+      Log.i(getClass().getSimpleName(), "RadDetService Timer started....");
+    }
+    
+    
+    //start the processing, the actual work, getting config params, get data from network etc
+    private void doServiceWork()
+    {
+        //do something whatever you want
+        //TODO: we implement our stuff here    
+    }
+    
+    
+    //shutting down the service
+    
+    private void _shutdownService()
+    {
+      if (timer != null) timer.cancel();
+      Log.i(getClass().getSimpleName(), "Timer stopped...");
+    }
+}
