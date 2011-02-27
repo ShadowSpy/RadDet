a77034bd72a85884a673e6ee596c7603385c7fe7
chencaustin <chencaustin@gmail.com>
2011-02-26 18:03:25 -0800

    Add sig strength, but doesnt work together yet

---
+++ src/edu/ucla/raddet/collector/DataCollector.java
@@ -0,0 +1,146 @@
+package edu.ucla.raddet.collector;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+
+
+//Android Service which collects radiation information, as well as GPS information
+public class DataCollector extends Service{
+
+	public static final int UPDATE_INTERVAL = 100;	//Frequency of GPS location updates
+		
+	private TelephonyManager Tel;
+	private LocationManager locManager;
+	private FileOutputStream fOut;
+	private OutputStreamWriter osw;
+	private SignalStrength sigstrength;
+	
+	private int mGsmSignalStrength; // Valid values are (0-31, 99) as defined in TS 27.007 8.5
+	
+	private int gpsStatus;
+	private int networkStatus;
+	private Location bestLocation;
+	
+	private static final String TAG = "DataCollector";
+	
+	private PhoneStateListener signalListener = new PhoneStateListener() {
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+             Log.i("Signal Strength: ",String.valueOf(signalStrength.getGsmSignalStrength()));
+        }
+    }; 
+
+	private LocationListener locListener = new LocationListener() {
+		public void onLocationChanged(Location loc) {
+			//Keep track of best location
+			//Having a location > no location
+			if (bestLocation == null)
+				bestLocation = loc;
+			//GPS Location > Network Location
+			else if (bestLocation.getProvider().equals(LocationManager.NETWORK_PROVIDER) &&
+					 loc.getProvider().equals(LocationManager.GPS_PROVIDER))
+				bestLocation = loc;
+			//More accuracy > Less accuracy
+			else if (bestLocation.getProvider().equals(loc.getProvider())) {
+				if (loc.getAccuracy() >= bestLocation.getAccuracy())
+					bestLocation = loc;
+			}
+			//Get Signal Strength
+			/*int strength = sigstrength.getGsmSignalStrength();
+			String sstrength = "SIGNAL STRENGTH!!!:" + String.valueOf(strength);
+			Log.i(TAG, sstrength);*/
+		}
+		public void onProviderDisabled(String provider) {
+		}
+		public void onProviderEnabled(String provider) {
+		}
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			if (provider.equals(LocationManager.GPS_PROVIDER))
+				gpsStatus = status;
+			else if (provider.equals(LocationManager.NETWORK_PROVIDER))
+				networkStatus = status;
+			
+			if (gpsStatus == LocationProvider.TEMPORARILY_UNAVAILABLE ||
+			   (gpsStatus == LocationProvider.OUT_OF_SERVICE && 
+				networkStatus == LocationProvider.TEMPORARILY_UNAVAILABLE)) {
+				//Send best location update
+				if (bestLocation != null) {
+					try {
+						String s = "Coordinates: " + bestLocation.getLatitude() + ", " + bestLocation.getLongitude();
+						s += " at timestamp " + bestLocation.getTime();
+						osw.write(s);
+						Log.d(TAG, s);
+					} catch (IOException e) {
+						Log.e(TAG, "Could not open output file");
+						e.printStackTrace();
+					}
+				}
+				bestLocation = null;
+			}
+		}
+	};
+	
+	public void onCreate() {
+		
+		//GPS Signal Strength
+        Tel = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        Tel.listen(signalListener ,PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+		
+		locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_INTERVAL, 0, locListener);
+		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, UPDATE_INTERVAL, 0, locListener);
+		try {
+			fOut = openFileOutput("samplefile.txt", MODE_WORLD_READABLE);
+		} catch (FileNotFoundException e) {
+			Log.e(TAG, "Could not open file output");
+			e.printStackTrace();
+		}
+		osw = new OutputStreamWriter(fOut);
+		Log.i(TAG, "DataCollector started");
+		Menu.started = true;
+	}
+	
+	public void onDestroy() {
+		locManager.removeUpdates(locListener);
+		try {
+			osw.flush();
+			osw.close();
+		} catch (IOException e) {
+			Log.e(TAG, "Could not close file writer");
+			e.printStackTrace();
+		}
+		Log.i(TAG, "DataCollector stopped");
+		Menu.started = false;
+	}
+
+	//TODO: Austin--Write function and figure out what the return value should actually be
+	public int getRadiation(SignalStrength sig) {
+			int strength = sig.getGsmSignalStrength();
+			return strength;
+	}
+	
+	public String getSig(SignalStrength sig){
+		String s = sig.getClass().getName() + '@' + Integer.toHexString(hashCode());
+		return s;
+	}
+	
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+}
---
+++ src/edu/ucla/raddet/collector/Menu.java
@@ -0,0 +1,68 @@
+package edu.ucla.raddet.collector;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Menu extends Activity {
+	
+	public static boolean started = false;
+	
+	LinearLayout node;
+    TextView tv;
+    Button button;	//Turns the service on or off
+    Intent dataCollectorIntent;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tv = new TextView(this);
+        button = new Button(this);
+        node = new LinearLayout(this);
+        node.setOrientation(LinearLayout.VERTICAL);
+        
+        if (started) {
+			tv.setText("Service is started");
+	        button.setText("Stop");
+	    	button.setOnClickListener(stopListener);
+        }
+        else {
+			tv.setText("Service is stopped");
+	        button.setText("Start");
+	    	button.setOnClickListener(startListener);
+        }
+        
+        node.addView(tv);
+        node.addView(button);
+        
+        dataCollectorIntent = new Intent(this, DataCollector.class);
+
+	    setContentView(node);
+    }
+    
+    private View.OnClickListener startListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	tv.setText("Please wait...");
+        	startService(dataCollectorIntent);
+        	button.setText("Stop");
+        	tv.setText("Service is started");
+        	button.setOnClickListener(stopListener);
+        }
+    };
+    
+    private View.OnClickListener stopListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	tv.setText("Please wait...");
+        	stopService(dataCollectorIntent);
+        	button.setText("Start");
+        	tv.setText("Service is stopped");
+        	button.setOnClickListener(startListener);
+        }
+    };
+}
