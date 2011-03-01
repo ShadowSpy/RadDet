b5d1a0bfa9b2f682e64e3292b8aef669878bfdf1
Roman <changr@ucla.edu>
2011-02-28 17:56:01 -0800

    More clean up. File now in append mode.

--- src/edu/ucla/raddet/collector/DataCollector.java
+++ src/edu/ucla/raddet/collector/DataCollector.java
@@ -30,20 +30,19 @@ public class DataCollector extends Service{
 	private LocationManager locManager;
 	private FileOutputStream fOut;
 	private OutputStreamWriter osw;
-	private int sig; // Valid values are (0-31, 99) as defined in TS 27.007 8.5
-	
+
 	private int gpsStatus;
 	private int networkStatus;
+	private int sig; // Valid values are (0-31, 99) as defined in TS 27.007 8.5
 	private Location bestLocation;
 	
-	private static final String TAG = "DataCollector";
+	private static final String TAG = "RadDet";
 	
 	private PhoneStateListener signalListener = new PhoneStateListener() {
         public void onSignalStrengthsChanged(SignalStrength signalStrength) {
         	sig = signalStrength.getGsmSignalStrength();
-        	//Log.i("Signal Strength: ",String.valueOf(sig));
+        	Log.d(TAG, "New Signal Strength: " +sig);
         }
-        
     }; 
 
 	private LocationListener locListener = new LocationListener() {
@@ -62,14 +61,6 @@ public class DataCollector extends Service{
 					bestLocation = loc;
 			Log.d(TAG, "Best location is currently: " + bestLocation.getLatitude() + ", " + bestLocation.getLongitude());
 			}
-			//Get Signal Strength
-			/*int strength = sigstrength.getGsmSignalStrength();
-			String sstrength = "SIGNAL STRENGTH!!!:" + String.valueOf(strength);
-			Log.i(TAG, sstrength);*/
-		}
-		public void onProviderDisabled(String provider) {
-		}
-		public void onProviderEnabled(String provider) {
 		}
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			//super.onStatusChanged(provider, status, extras);
@@ -90,9 +81,10 @@ public class DataCollector extends Service{
 						s += "," + bestLocation.getTime() + ",";
 						s += sig + "\n";
 						osw.write(s);
-						Log.i(TAG, s + "Latitude,Longitude,Time,Signal");
+						osw.flush();
+						Log.i(TAG, "(Latitude,Longitude,Time,Signal)" + s);
 						
-						Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+						Toast.makeText(getApplicationContext(), "(Latitude,Longitude,Time,Signal)" + s, Toast.LENGTH_LONG).show();
 						
 					} catch (IOException e) {
 						Log.e(TAG, "Could not open output file");
@@ -102,50 +94,46 @@ public class DataCollector extends Service{
 				}
 			}
 		}
+
+		// Functions declared for sake of interface satisfaction
+		public void onProviderDisabled(String provider) {}
+		public void onProviderEnabled(String provider) {}
 	};
 	
 	public void onCreate() {
 		
-		//GPS Signal Strength
+		//Set up GPS Signal Strength
         Tel = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         Tel.listen(signalListener ,PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
 		
+        //Set up location manager
 		locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_INTERVAL, 0, locListener);
 		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, UPDATE_INTERVAL, 0, locListener);
+		
+		//Set up output file
 		try {
-			fOut = openFileOutput("current.txt", MODE_WORLD_READABLE);
+			fOut = openFileOutput("current.txt", MODE_WORLD_READABLE | MODE_APPEND);
 		} catch (FileNotFoundException e) {
 			Log.e(TAG, "Could not open file output");
 			e.printStackTrace();
 		}
 		osw = new OutputStreamWriter(fOut);
+		
 		Log.i(TAG, "DataCollector started");
-		Menu.started = true;
+		Menu.started = true;	//Notify Activity that service has started
 	}
 	
 	public void onDestroy() {
 		locManager.removeUpdates(locListener);
 		try {
-			osw.flush();
 			osw.close();
 		} catch (IOException e) {
 			Log.e(TAG, "Could not close file writer");
 			e.printStackTrace();
 		}
 		Log.i(TAG, "DataCollector stopped");
-		Menu.started = false;
-	}
-
-	//TODO: Austin--Write function and figure out what the return value should actually be
-	public int getRadiation(SignalStrength sig) {
-			int strength = sig.getGsmSignalStrength();
-			return strength;
-	}
-	
-	public String getSig(SignalStrength sig){
-		String s = sig.getClass().getName() + '@' + Integer.toHexString(hashCode());
-		return s;
+		Menu.started = false;	//Notify Activity that service has stopped
 	}
 	
 	public IBinder onBind(Intent intent) {
